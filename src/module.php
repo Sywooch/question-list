bbc4ea89192a1daad501218f3eb1c07e81e94747
Ilya Gribov <imilyagribov@gmail.com>
2016-02-01 11:30:14 +0300

    rename

---
+++ src/module.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace igribov\questionlist;
+
+class module extends \yii\base\Module
+{
+    public $controllerNamespace = 'igribov\questionlist\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+
+}
